028c4f5a7afbdd225f7bd156344625a2066c9c75
root <y.kawano0723@gmail.com>
2021-03-08 10:05:04 +0000

    add visualization

--- app/run.py
+++ app/run.py
@@ -1,4 +1,5 @@
 import json
+import pickle
 import plotly
 import pandas as pd
 
@@ -40,27 +41,63 @@ def index():
     
     # extract data needed for visuals
     # TODO: Below is an example - modify to extract data for your own visuals
-    genre_counts = df.groupby('genre').count()['message']
-    genre_names = list(genre_counts.index)
     
+    # categories usage rates
+    cat_titles = df.columns[4:].values
+    cat_rates = []
+    for title in cat_titles:
+        cat_rates.append([title, df[title].sum()  / df.shape[0]])
+    df_cat_rates = pd.DataFrame(cat_rates, columns=['cat', 'rate'])
+    df_cat_rates = df_cat_rates.sort_values('rate', ascending=False).reset_index(drop=True)
+    
+    cat_rates = df_cat_rates['rate'].values
+    categories=df_cat_rates['cat'].values
+
+    # Kinds number of message category
+    cat_kinds = df.iloc[:, 4:].sum(axis=1)
+    kinds_num = [str(x) for x in range(0, 15)]
+    kinds_num.append('15+')
+    kinds_counts = []
+    for i in range(0, 15):
+        kinds_counts.append(len(cat_kinds[cat_kinds == i]))
+    kinds_counts.append(len(cat_kinds[cat_kinds > 14]))
+
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
         {
             'data': [
                 Bar(
-                    x=genre_names,
-                    y=genre_counts
+                    x=categories,
+                    y=cat_rates
                 )
             ],
 
             'layout': {
                 'title': 'Distribution of Message Genres',
                 'yaxis': {
-                    'title': "Count"
+                    'title': "Rate"
                 },
                 'xaxis': {
-                    'title': "Genre"
+                    'title': "Categories"
+                }
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=kinds_num,
+                    y=kinds_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Kinds number in one message',
+                'yaxis': {
+                    'title': "Counts"
+                },
+                'xaxis': {
+                    'title': "Kinds"
                 }
             }
         }
